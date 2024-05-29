d689e0209b228e65aca6062ccbcb1edeb6ba7ef7
LoneWolf <imalkesara2001@gmail.com>
2024-05-29 20:24:33 +0530

    basic structure

---
+++ src/routes/Game.svelte
@@ -0,0 +1,29 @@
+<div class="game">
+    <div class="info">Timer</div>
+    <div class="grid-container">Grid</div>
+    <div class="info">result</div>
+</div>
+
+<style>
+    
+    .game{
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+    }
+
+    .info{
+        width: 80vmin;
+        height: 10vmin;
+        background-color: rgb(185, 61, 3);
+    }
+
+    .grid-container{
+        width: 80vmin;
+        background-color: rgb(129, 0, 0);
+        height: 50vmin;
+    }
+
+</style>
