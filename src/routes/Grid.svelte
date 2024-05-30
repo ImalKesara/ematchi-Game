c2d1f5b02f36cda0e2347ac6fe53c24a8b5380da
LoneWolf <imalkesara2001@gmail.com>
2024-05-30 14:01:30 +0530

    set 2 varioble and index

--- src/routes/Grid.svelte
+++ src/routes/Grid.svelte
@@ -2,10 +2,19 @@
   import Square from "./Square.svelte";
   export let grid :string[];
 
+  let a = -1;        // card 1
+  let b = -1;       // card 2
+
 </script>
 <div class="grid">
-    {#each grid as emoji}
-        <Square {emoji}/>
+    {#each grid as emoji,i}
+        <Square {emoji}  on:click= {()=>{
+            if(a === -1 && b === -1){
+                a = i;
+            }
+        }}
+        selected = {a === i || b === i}
+        />
     {/each}
 </div>
 
