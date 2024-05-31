1193bd662ee0ffa2d39d29af0f89e68b780d84cd
LoneWolf <imalkesara2001@gmail.com>
2024-05-31 21:56:52 +0530

    export send, receive

--- src/routes/transition.ts
+++ src/routes/transition.ts
@@ -1,4 +1,4 @@
 import { crossfade } from 'svelte/transition';
 import { cubicOut } from 'svelte/easing';
 
-const [send, receive] = crossfade({ duration: 400, easing: cubicOut });
+export const [send, receive] = crossfade({ duration: 400, easing: cubicOut });
